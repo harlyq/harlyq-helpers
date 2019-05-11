588feed0cb13c1366baa5d6421f8b5fa2a1ae81f
harlyq <celerity252@gmail.com>
2019-05-11 20:24:46 +0930

    fixed affine4 rotate and scale functions, added multiply and invert
    - added multiply, invert functions and tests

--- src/affine4.js
+++ src/affine4.js
@@ -30,65 +30,147 @@ export function translateXYZ(out, aff, t) {
   return out
 }
 
-/** @type {<T extends Affine4, TA extends Affine4>(out: T, aff: TA, rad: Radians) => T} */
-export function rotateX(out, aff, rad) {
-  for (let i = 0; i < aff.length; i++) {
-    out[i] = aff[i]
-  }
+/** @type {<T extends Affine4, TT extends VecXYZ>(out: T, t: TT) => T} */
+export function makeTranslateXYZ(out, t) {
+  out.fill(0)
+
+  out[12] = t.x
+  out[13] = t.y
+  out[14] = t.z
+  out[0] = out[5] = out[10] = out[15] = 1
+
+  return out
+}
+
+/** @type {<T extends Affine4, TA extends Affine4>(out: T, rad: Radians) => T} */
+export function makeRotateX(out, rad) {
+  out.fill(0)
 
   const cs = Math.cos(rad)
   const ss = Math.sin(rad)
-  out[5] *= cs
-  out[6] *= ss
-  out[9] *= -ss
-  out[10] *= cs
+  out[5] = cs
+  out[6] = ss
+  out[9] = -ss
+  out[10] = cs
+  out[0] = out[15] = 1
 
   return out
 }
 
-/** @type {<T extends Affine4, TA extends Affine4>(out: T, aff: TA, rad: Radians) => T} */
-export function rotateY(out, aff, rad) {
-  for (let i = 0; i < aff.length; i++) {
-    out[i] = aff[i]
-  }
+/** @type {<T extends Affine4>(out: T, rad: Radians) => T} */
+export function makeRotateY(out, rad) {
+  out.fill(0)
 
   const cs = Math.cos(rad)
   const ss = Math.sin(rad)
-  out[0] *= cs
-  out[2] *= -ss
-  out[8] *= ss
-  out[10] *= cs
+  out[0] = cs
+  out[2] = -ss
+  out[8] = ss
+  out[10] = cs
+  out[5] = out[15] = 1
 
   return out
 }
 
-/** @type {<T extends Affine4, TA extends Affine4>(out: T, aff: TA, rad: Radians) => T} */
-export function rotateZ(out, aff, rad) {
-  for (let i = 0; i < aff.length; i++) {
-    out[i] = aff[i]
-  }
+/** @type {<T extends Affine4>(out: T, rad: Radians) => T} */
+export function makeRotateZ(out, rad) {
+  out.fill(0)
 
   const cs = Math.cos(rad)
   const ss = Math.sin(rad)
-  out[0] *= cs
-  out[1] *= ss
-  out[4] *= -ss
-  out[5] *= cs
+  out[0] = cs
+  out[1] = ss
+  out[4] = -ss
+  out[5] = cs
+  out[10] = out[15] = 1
 
   return out
 }
 
-/** @type {<T extends Affine4, TA extends Affine4, TS extends VecXYZ>(out: T, aff: TA, scale: TS) => T} */
-export function scaleXYZ(out, aff, scale) {
-  for (let i = 0; i < aff.length; i++) {
-    out[i] = aff[i]
-  }
+/** @type {<T extends Affine4, TS extends VecXYZ>(out: T, scale: TS) => T} */
+export function makeScaleXYZ(out, scale) {
+  out.fill(0)
 
-  out[0] *= scale.x
-  out[5] *= scale.y
-  out[10] *= scale.z
+  out[0] = scale.x
+  out[5] = scale.y
+  out[10] = scale.z
+  out[15] = 1
+
   return out
 }
+
+/** @type {<T extends Affine4, TA extends Affine4, TB extends Affine4>(out: T, affA: TA, affB: TB) => T} */
+export function multiply(out, affA, affB) {
+  const a11 = affA[0], a21 = affA[1], a31 = affA[2]
+  const a12 = affA[4], a22 = affA[5], a32 = affA[6]
+  const a13 = affA[8], a23 = affA[9], a33 = affA[10]
+  const a14 = affA[12], a24 = affA[13], a34 = affA[14]
+
+  const b11 = affB[0], b21 = affB[1], b31 = affB[2]
+  const b12 = affB[4], b22 = affB[5], b32 = affB[6]
+  const b13 = affB[8], b23 = affB[9], b33 = affB[10]
+  const b14 = affB[12], b24 = affB[13], b34 = affB[14]
+
+  out[0] = a11*b11 + a12*b21 + a13*b31
+  out[1] = a21*b11 + a22*b21 + a23*b31
+  out[2] = a31*b11 + a32*b21 + a33*b31
+
+  out[4] = a11*b12 + a12*b22 + a13*b32
+  out[5] = a21*b12 + a22*b22 + a23*b32
+  out[6] = a31*b12 + a32*b22 + a33*b32
+
+  out[8] = a11*b13 + a12*b23 + a13*b33
+  out[9] = a21*b13 + a22*b23 + a23*b33
+  out[10] = a31*b13 + a32*b23 + a33*b33
+
+  out[12] = a11*b14 + a12*b24 + a13*b34 + a14
+  out[13] = a21*b14 + a22*b24 + a23*b34 + a24
+  out[14] = a31*b14 + a32*b24 + a33*b34 + a34
+
+  out[3] = out[7] = out[11] = 0
+  out[15] = 1
+
+  return out
+}
+
+/** @type {<T extends Affine4, TA extends Affine4>(out: T, aff: TA) => T} */
+export function invert(out, aff) {
+  const n11 = aff[0], n21 = aff[1], n31 = aff[2]
+  const n12 = aff[4], n22 = aff[5], n32 = aff[6]
+  const n13 = aff[8], n23 = aff[9], n33 = aff[10]
+  const tx = aff[12], ty = aff[13], tz = aff[14]
+
+  const t11 = n33 * n22 - n32 * n23
+  const t12 = n32 * n13 - n33 * n12
+  const t13 = n23 * n12 - n22 * n13
+
+  const det = n11 * t11 + n21 * t12 + n31 * t13
+  const invDet = 1/det
+
+  // invert the rotation matrix
+  const m11 = t11 * invDet
+  const m21 = ( n31 * n23 - n33 * n21 ) * invDet
+  const m31 = ( n32 * n21 - n31 * n22 ) * invDet
+
+  const m12 = t12 * invDet
+  const m22 = ( n33 * n11 - n31 * n13 ) * invDet
+  const m32 = ( n31 * n12 - n32 * n11 ) * invDet
+
+  const m13 = t13 * invDet
+  const m23 = ( n21 * n13 - n23 * n11 ) * invDet
+  const m33 = ( n22 * n11 - n21 * n12 ) * invDet
+
+  out[0] = m11, out[1] = m21, out[2] = m31, out[3] = 0
+  out[4] = m12, out[5] = m22, out[6] = m32, out[7] = 0
+  out[8] = m13, out[9] = m23, out[10] = m33, out[11] = 0
+  out[12] = -m11*tx - m12*ty - m13*tz
+  out[13] = -m21*tx - m22*ty - m23*tz
+  out[14] = -m31*tx - m32*ty - m33*tz
+  out[15] = 1
+
+  return out
+}
+
 
 /** @type {<T extends VecXYZ, TA extends Affine4, TV extends VecXYZ>(out: T, aff: TA, v: TV) => T} */
 export function multiplyVecXYZ(out, aff, v) {
--- test/test-affine4.js
+++ test/test-affine4.js
@@ -2,13 +2,73 @@ import test from "tape"
 import * as affine4 from "../src/affine4.js"
 
 const identity = () => new Float32Array([1,0,0,0, 0,1,0,0, 0,0,1,0, 0,0,0,1])
-const arraySimilar = (a,b) => [].every.call(a, (x,i) => Math.abs(x - b[i]) < 1e-5)
+const arraySimilar = (a,b,epsilon = 1e-5) => [].every.call(a, (x,i) => Math.abs(x - b[i]) < epsilon)
 const RAD45 = 45/180*Math.PI
 const COSIN45 = Math.cos(RAD45)
 const SIN45 = Math.sin(RAD45)
 
 test("affine4.create", (t) => {
   t.deepEquals(affine4.create(), identity(), "create")
+  t.end()
+})
+
+test("affine4.translateXYZ", (t) => {
+  let out = affine4.create()
+  t.deepEquals(affine4.translateXYZ(out, identity(), {x:0,y:0,z:0}), identity(), "no translation")
+  t.deepEquals(affine4.translateXYZ(out, [1,2,3,4, 5,6,7,8, 9,10,11,12, 0,0,0,1], {x:-1,y:2,z:-.75}), [1,2,3,4, 5,6,7,8, 9,10,11,12, -1,2,-.75,1], "translation")
+  t.end()
+})
+
+test("affine4.makeTranslateXYZ", (t) => {
+  let out = affine4.create()
+  t.deepEquals(affine4.makeTranslateXYZ(out, {x:0,y:0,z:0}), identity(), "no translation")
+  t.deepEquals(affine4.makeTranslateXYZ(out, {x:-2,y:3,z:-4.25}), [1,0,0,0, 0,1,0,0, 0,0,1,0, -2,3,-4.25,1], "translation")
+  t.end()
+})
+
+test("affine4.makeRotateX", (t) => {
+  let out = affine4.create()
+  let outB = affine4.create()
+  t.deepEquals(affine4.makeRotateX(out, 0), identity(), "no rotation")
+  t.deepEquals(affine4.makeRotateX(out, 0.2), affine4.makeRotateX(outB, 2*Math.PI + 0.2), "rotation")
+  t.end()
+})
+
+test("affine4.makeRotateY", (t) => {
+  let out = affine4.create()
+  let outB = affine4.create()
+  t.deepEquals(affine4.makeRotateY(out, 0), identity(), "no rotation")
+  t.deepEquals(affine4.makeRotateY(out, 0.2), affine4.makeRotateY(outB, 2*Math.PI + 0.2), "rotation")
+  t.end()
+})
+
+test("affine4.makeRotateZ", (t) => {
+  let out = affine4.create()
+  let outB = affine4.create()
+  t.deepEquals(affine4.makeRotateZ(out, 0), identity(), "no rotation")
+  t.deepEquals(affine4.makeRotateZ(out, 0.2), affine4.makeRotateZ(outB, 2*Math.PI + 0.2), "rotation")
+  t.end()
+})
+
+test("affine4.invert", (t) => {
+  let out = affine4.create()
+  let outB = affine4.create()
+  t.deepEquals(affine4.invert(out, identity()), identity(), "identity")
+  t.deepEquals(affine4.invert(out, affine4.makeTranslateXYZ(out, {x:1.3, y:-9, z:2})), affine4.makeTranslateXYZ(outB, {x:-1.3, y:9, z:-2}), "translation")
+  t.deepEquals(affine4.invert(out, affine4.makeRotateX(out, 1.3)), affine4.makeRotateX(outB, -1.3), "rotation")
+  t.deepEquals(affine4.invert(out, affine4.makeScaleXYZ(out, {x:9, y:.5, z:-3})), affine4.makeScaleXYZ(outB, {x:1/9, y:1/.5, z:1/-3}), "scale")
+  t.ok( arraySimilar( affine4.invert(out, [-.34,-.189,.10,0, .30,-.69,-.28,0, .23,-.12,.54,0, .07,7.8,15.3,1]), [-2.10,.47,.63,0, -1.17,-1.07,-.34,0, .64,-.44,1.51,0, -.43,15,-20.48,1], 0.02 ), "random matrix")
+  t.end()
+})
+
+test("affine4.multiply", (t) => {
+  let out = affine4.create()
+  let outB = affine4.create()
+  let outC = affine4.create()
+  t.deepEquals(affine4.multiply(out, identity(), identity()), identity(), "identity")
+  t.ok( arraySimilar(affine4.multiply(out, [-.34,-.189,.10,0, .30,-.69,-.28,0, .23,-.12,.54,0, .07,7.8,15.3,1], affine4.invert(out, [-.34,-.189,.10,0, .30,-.69,-.28,0, .23,-.12,.54,0, .07,7.8,15.3,1])), identity() ), "A*inv(A)")
+  t.ok( arraySimilar( affine4.multiply(out, affine4.makeRotateX(out, 0.25), affine4.makeRotateX(outB, 1.25)), affine4.makeRotateX(outC, 1.5), 0.01 ), "multiply rotation")
+  t.deepEquals(affine4.multiply(out, affine4.makeTranslateXYZ(out, {x:1,y:2,z:3}), affine4.makeTranslateXYZ(outB, {x:-1,y:-4,z:-12})), affine4.makeTranslateXYZ(outC, {x:0,y:-2,z:-9}), "multiply translation")
   t.end()
 })
 
