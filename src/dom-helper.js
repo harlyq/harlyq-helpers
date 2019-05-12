1aaef494098363d5ce241706f35b12755eb4c2f1
harlyq <celerity252@gmail.com>
2019-05-12 10:28:12 +0930

    added domHelper.getDebugName() and domHelper.applyNodeMutations()
    - getDebugName() will return the element name as <type>#<id>.<class>
    - applyNodeMutations() will take a set of MutationObserver mutations and apply them to a list of elements for a given selector (quicker than performing querySelectorAll() again)

--- src/dom-helper.js
+++ src/dom-helper.js
@@ -13,3 +13,24 @@ export function findMatchingAncestor(node, selector) {
   }
   return parent && 'matches' in parent ? parent : undefined
 }
+
+export function getDebugName(el) {
+  return el.tagName.toLowerCase() + (el.id ? '#' + el.id : '') + (el.classList.length > 0 ? '.' + el.classList.join('.') : '')
+}
+
+// for large DOMs with few changes, checking the mutations is faster than querySelectorAll()
+export function applyNodeMutations(elements, mutations, selector) {
+  for (let mutation of mutations) {
+    for (let addedEl of mutation.addedNodes) {
+      if (addedEl.matches(selector)) {
+        elements.push(addedEl)
+      }
+    }
+
+    for (let removedEl of mutation.removedNodes) {
+      if (removedEl.matches(selector)) {
+        elements.splice(elements.indexOf(removedEl), 1)
+      }
+    }
+  }
+}
