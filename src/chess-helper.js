f63159df8d2b6c1f5c8bde54b720b2a8a4143d68
harlyq <celerity252@gmail.com>
2019-12-02 15:07:30 +1030

    added isCapture to isMovePossible()
    - isMovePossible considers capture (for pawns)
    - exposed decodeFile() and decodeRank() to decode the file and rank

--- src/chess-helper.js
+++ src/chess-helper.js
@@ -1,9 +1,17 @@
 const SAN_REGEX_STR = /([RNBKQ]?[a-h]?[1-8]?)(x?)([a-h][1-8])(=[RNBQ])?([\#|\+]?)/
 const SAN_REGEX = new RegExp(SAN_REGEX_STR)
 
+export function decodeFile(fileStr) {
+  return fileStr.charCodeAt(0) - 96
+}
+
+export function decodeRank(rankStr) {
+  return rankStr.charCodeAt(0) - 48
+}
+
 // rank "a"->"h" => 1->8, file "1"->"8" -> 1->8
 export function coordToFileRank(coord) {
-  return coord.length === 2 ? [coord.charCodeAt(0) - 96, coord.charCodeAt(1) - 48] : undefined
+  return coord.length === 2 ? [decodeFile(coord[0]), decodeRank(coord[1])] : undefined
 }
 
 // rank 1->8 => "a"->"h", file 1->8 -> "1"->"8"
@@ -14,7 +22,7 @@ export function fileRankToCoord(rank, file) {
 export function decodeSAN(san) {
   const parts = san.match(SAN_REGEX)
   return parts ? {
-    pieceSAN: parts[1],
+    piece: parts[1],
     capture: parts[2] === "x",
     to: parts[3],
     promotion: parts[4] ? parts[4][1] : "",
@@ -26,7 +34,7 @@ export function decodeSAN(san) {
 // Note, it may include illegal moves e.g. en passant without another pawn,
 // or moving through other pieces
 // Bote, both rank and file are numbers between 1 and 8 inclusive
-export function isMovePossible(code, pieceFile, pieceRank, toFile, toRank) {
+export function isMovePossible(code, pieceFile, pieceRank, toFile, toRank, isCapture) {
   if (pieceFile === toFile && pieceRank === toRank) {
     return false
   }
@@ -35,7 +43,7 @@ export function isMovePossible(code, pieceFile, pieceRank, toFile, toRank) {
     case "p":
     case "P":
       const isBlack = code === code.toLowerCase()
-      if (pieceFile === toFile) {
+      if (pieceFile === toFile && !isCapture) {
         if (isBlack && pieceRank === 7) {
           return toRank === 6 || toRank === 5
         } else if (!isBlack && pieceRank === 2) {
@@ -43,7 +51,7 @@ export function isMovePossible(code, pieceFile, pieceRank, toFile, toRank) {
         } else {
           return toRank === pieceRank + (isBlack ? -1 : 1)
         }
-      } else if (pieceFile - 1 === toFile || pieceFile + 1 === toFile) {
+      } else if ( isCapture && (pieceFile - 1 === toFile || pieceFile + 1 === toFile) ) {
         return toRank === pieceRank + (isBlack ? -1 : 1)
       }
       return false
