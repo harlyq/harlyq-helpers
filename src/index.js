84d30157363479cf3980a9d6de4c1638305f1dc4
harlyq <celerity252@gmail.com>
2020-05-02 13:42:37 +0930

    simplified index.js
    - replaced import/export combos with a single export
    - removed peerDependencies

--- src/index.js
+++ src/index.js
@@ -1,54 +1,27 @@
-import * as affine4 from "./affine4.js"
-import * as aframeHelper from "./aframe-helper.js"
-import * as attribute from "./attribute.js"
-import * as cardHelper from "./card-helper.js"
-import * as chessHelper from "./chess-helper.js"
-import * as closestPoint from "./closest-point.js"
-import * as domHelper from "./dom-helper.js"
-import * as extent from "./extent.js"
-import * as hull from "./hull.js"
-import * as instanced from "./instanced.js"
-import * as interpolation from "./interpolation.js"
-import * as intersection from "./intersection.js"
-import * as jsonHelper from "./json-helper.js"
-import * as nafHelper from "./naf-helper.js"
-// import * as nodeHelper from "./node-helper.js"
-import * as overlap from "./overlap.js"
-import * as proximity from "./proximity.js"
-import * as pseudorandom from "./pseudorandom.js"
-import * as quatxyzw from "./quatxyzw.js"
-import * as rgbcolor from "./rgbcolor.js"
-import * as selector from "./selector.js"
-import * as slidingHelper from "./sliding-helper.js"
-import * as utils from "./utils.js"
-import * as threeHelper from "./three-helper.js"
-import * as vecxyz from "./vecxyz.js"
-import * as vertex3 from "./vertex3.js"
-
 // exports will tree shake correctly (unless bundled)
-export { affine4 }
-export { aframeHelper }
-export { attribute }
-export { cardHelper }
-export { chessHelper }
-export { closestPoint }
-export { domHelper }
-export { extent }
-export { instanced }
-export { interpolation }
-export { intersection }
-export { jsonHelper }
-export { hull }
-export { nafHelper }
-// export { nodeHelper }
-export { overlap }
-export { proximity }
-export { pseudorandom }
-export { quatxyzw }
-export { rgbcolor }
-export { selector }
-export { slidingHelper }
-export { utils }
-export { threeHelper }
-export { vecxyz }
-export { vertex3 }
+export * as affine4 from "./affine4.js"
+export * as aframeHelper from "./aframe-helper.js"
+export * as attribute from "./attribute.js"
+export * as cardHelper from "./card-helper.js"
+export * as chessHelper from "./chess-helper.js"
+export * as closestPoint from "./closest-point.js"
+export * as domHelper from "./dom-helper.js"
+export * as extent from "./extent.js"
+export * as hull from "./hull.js"
+export * as instanced from "./instanced.js"
+export * as interpolation from "./interpolation.js"
+export * as intersection from "./intersection.js"
+export * as jsonHelper from "./json-helper.js"
+export * as nafHelper from "./naf-helper.js"
+// export * as nodeHelper from "./node-helper.js"
+export * as overlap from "./overlap.js"
+export * as proximity from "./proximity.js"
+export * as pseudorandom from "./pseudorandom.js"
+export * as quatxyzw from "./quatxyzw.js"
+export * as rgbcolor from "./rgbcolor.js"
+export * as selector from "./selector.js"
+export * as slidingHelper from "./sliding-helper.js"
+export * as utils from "./utils.js"
+export * as threeHelper from "./three-helper.js"
+export * as vecxyz from "./vecxyz.js"
+export * as vertex3 from "./vertex3.js"
